00f25f8fb99a9c1e702f4444f71850d16de20cbd
jeycsonlopez@gmail.com <jeycsonlopez@gmail.com>
2026-06-11 20:20:03 -0600

    creando db

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from sqlalchemy.orm import sessionmaker, Session
 # 1. Configuración de Base de Datos (SQLite)
 # En Render usaremos una ruta dentro del volumen persistente, localmente se creará en la raíz.
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./comments.db")
-
+if DATABASE_URL.startswith("sqlite:////data/"):
+    os.makedirs("/data", exist_ok=True)
+    
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
